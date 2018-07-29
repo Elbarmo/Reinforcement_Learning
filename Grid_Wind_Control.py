bf9ac30f9a9e4c8a96cbba6910caac7bccef521f
Elias Barba Moral <elias.barbamoral@p3-group.com>
2018-07-29 16:41:17 +0200

    Structure and further clean of the code

--- Grid_Wind_Control.py
+++ Grid_Wind_Control.py
@@ -5,11 +5,10 @@ Created on Wed Jul 18 13:48:44 2018
 @author: eia
 """
 from random import randint
-from numpy.linalg import inv
 import numpy as np
 np.set_printoptions(threshold=np.nan)
 
-######## MC CONTROL ##########
+######## World definition #######
 
 states = np.arange(25).reshape(5,5)
 action_list = ["north","south","east","west"]
@@ -17,21 +16,58 @@ action_list = ["north","south","east","west"]
 terminate_state_plus = {"x":3,"y":3}
 terminate_state_minus = {"x":1,"y":1}
 
+######## Functions ########
+
+def nice_display(policy):
+    '''
+    param: policy
+    return: character array displaying the action with highest score
+    '''
+    charar = np.chararray((5, 5),itemsize = 5)
+    for state in range(25):
+        row = int(state/5)
+        col = state-row*5
+        charar[row,col] = action_list[policy[:,state].tolist().index(max(policy[:,state]))]
+    return charar
+
+
 def find_0_array(value):
     if value == 0:
         return 1
     else:
         return 0
 
-def sample_from_policy(policy,state_ind):
-    return np.random.choice(4, 1, p=policy[:,state_ind])[0]
-
 def normalize_policy(policy):
+    '''
+    
+    param: policy
+    return: policy normalized
+    '''
     policy = np.fabs(policy)
     norm = np.sum(policy,axis = 0)  
     return policy/norm
 
+def e_greedy_action(state_ind,policy):
+    '''
+    Apply 0.5 greedy policy to choose the next action to take
+    param: state_index
+    param: policy
+    return: action
+    '''
+    rand = randint(0,1)
+    if rand == 0:
+        action = randint(0,3)
+    else:
+        action = policy[:,state_ind].tolist().index(max(policy[:,state_ind]))
+    return action
+
 def calculate_new_state(state,action):
+    '''
+    From being in a state and taking an action, calculate the next state
+    param: state
+    param: action
+    return: state'
+    '''
         
     if action == 0:
         state["y"] -= 1
@@ -64,7 +100,11 @@ def calculate_new_state(state,action):
             
     return state
 
-def MC_walk(policy):
+def sample_init_state():
+    '''
+    Choose the initial state randomly
+    return: init_state
+    '''
     sample = True
     while sample:
         init_state={"x":randint(0,4),"y":randint(0,4)}
@@ -74,8 +114,16 @@ def MC_walk(policy):
             sample = True
         else:
             sample = False
+    return init_state
+
+
+######## MC CONTROL ##########
+
+def MC_walk(policy,find_0_array_vec):
+    init_state = sample_init_state()
         
     count_states_action = np.zeros((4,25))
+    
     state = init_state
     state_ind = states[state["y"],state["x"]]
     walking = True
@@ -92,9 +140,6 @@ def MC_walk(policy):
         state = calculate_new_state(state,action)
         state_ind = states[state["y"],state["x"]]
         if init_state["x"] == terminate_state_plus["x"] and init_state["y"] == terminate_state_plus["y"]:
-            if debug:
-                print("good")
-                print(count_states_action)
             reward = np.ones((4,25))
             walking = False
         elif init_state["x"] == terminate_state_minus["x"] and init_state["y"] == terminate_state_minus["y"]:
@@ -114,38 +159,11 @@ def MC_walk(policy):
     
     return policy
 
-def e_greedy_action(state_ind,policy):
-    rand = randint(0,1)
-    if rand == 0:
-        action = randint(0,3)
-    else:
-        action = policy[:,state_ind].tolist().index(max(policy[:,state_ind]))
-    return action
-
-policy = np.ones((4,25))/4
-        
-find_0_array_vec = np.vectorize(find_0_array)
-debug = False
-for i in range(100000):
-    if debug:
-        print("")
-        print(i)
-        print(policy)
-    policy = MC_walk(policy)
-
 
 ######## TD 0 CONTROL(SARSA) ##########
 
 def SARSA_eval(policy,count_states_action):
-    sample = True
-    while sample:
-        init_state={"x":randint(0,4),"y":randint(0,4)}
-        if init_state["x"] == terminate_state_plus["x"] and init_state["y"] == terminate_state_plus["y"]:
-            sample = True
-        elif init_state["x"] == terminate_state_minus["x"] and init_state["y"] == terminate_state_minus["y"]:
-            sample = True
-        else:
-            sample = False
+    init_state = sample_init_state()
             
     state = init_state
     state_ind = states[state["y"],state["x"]]
@@ -162,10 +180,6 @@ def SARSA_eval(policy,count_states_action):
         new_action = e_greedy_action(new_state_ind,policy)
         
         if new_state["x"] == terminate_state_plus["x"] and new_state["y"] == terminate_state_plus["y"]:
-            
-            if debug:
-                print("good")
-                print(count_states_action)
             reward = 1
             walking = False
             
@@ -184,31 +198,13 @@ def SARSA_eval(policy,count_states_action):
         state_ind = states[state["y"],state["x"]]
         action = new_action
         
-    return policy
+    return policy    
 
-policy = np.ones((4,25))/4
-debug = False
-count_states_action = np.zeros((4,25))
-for i in range(100000):
-    if debug:
-        print("")
-        print(i)
-        print(policy)
-    policy = SARSA_eval(policy,count_states_action)
-    
 
 ######## TD Lambda CONTROL(SARSA) ##########
 
-def SARSA_lambda_eval(policy,lambda_,count_states_action,count_good ,count_bad):
-    sample = True
-    while sample:
-        init_state={"x":randint(0,4),"y":randint(0,4)}
-        if init_state["x"] == terminate_state_plus["x"] and init_state["y"] == terminate_state_plus["y"]:
-            sample = True
-        elif init_state["x"] == terminate_state_minus["x"] and init_state["y"] == terminate_state_minus["y"]:
-            sample = True
-        else:
-            sample = False
+def SARSA_lambda_eval(policy,lambda_,count_states_action):
+    init_state = sample_init_state()
             
     elegibility = np.zeros((4,25))
     state = init_state
@@ -225,21 +221,14 @@ def SARSA_lambda_eval(policy,lambda_,count_states_action,count_good ,count_bad):
         new_action = e_greedy_action(new_state_ind,policy)
         
         if new_state["x"] == terminate_state_plus["x"] and new_state["y"] == terminate_state_plus["y"]:
-            
-            if debug:
-                print("good")
-                #print(count_states_action)
+
             reward = 10
             walking = False
-            count_good += 1
             
         elif new_state["x"] == terminate_state_minus["x"] and new_state["y"] == terminate_state_minus["y"]:
-            if debug:
-                print("bad")
-                #print(count_states_action)
+
             reward = 0
             walking = False
-            count_bad += 1
             
         else:            
             reward = 0
@@ -254,46 +243,13 @@ def SARSA_lambda_eval(policy,lambda_,count_states_action,count_good ,count_bad):
         state_ind = states[state["y"],state["x"]]
         action = new_action
         
-    return policy,count_good,count_bad,count_states_action
+    return policy,count_states_action
 
-policy = np.ones((4,25))
-lambda_ = 0.9
-debug = False
-count_good = 0
-count_bad = 0
-count_states_action = np.zeros((4,25))
-for i in range(100000):
-    if debug:
-        print("")
-        print(i)
-        print(policy)
-    policy,count_good,count_bad,count_states_action = SARSA_lambda_eval(policy,lambda_,count_states_action,count_good, count_bad)
-
-policy = normalize_policy(policy)
-
-
-def nice_display(policy):
-    charar = np.chararray((5, 5),itemsize = 5)
-    for state in range(25):
-        row = int(state/5)
-        col = state-row*5
-        charar[row,col] = action_list[policy[:,state].tolist().index(max(policy[:,state]))]
-    return charar
         
-optimal_policy = nice_display(policy)
 ######## Off-line CONTROL (SARSAMAX) ##########
 
-
-def SARSAmax(policy,count_states_action,count_good ,count_bad):
-    sample = True
-    while sample:
-        init_state={"x":randint(0,4),"y":randint(0,4)}
-        if init_state["x"] == terminate_state_plus["x"] and init_state["y"] == terminate_state_plus["y"]:
-            sample = True
-        elif init_state["x"] == terminate_state_minus["x"] and init_state["y"] == terminate_state_minus["y"]:
-            sample = True
-        else:
-            sample = False
+def SARSAmax(policy,count_states_action):
+    init_state = sample_init_state()
             
     state = init_state
     state_ind = states[state["y"],state["x"]]
@@ -309,10 +265,6 @@ def SARSAmax(policy,count_states_action,count_good ,count_bad):
         new_action = e_greedy_action(new_state_ind,policy)
         
         if new_state["x"] == terminate_state_plus["x"] and new_state["y"] == terminate_state_plus["y"]:
-            
-            if debug:
-                print("good")
-                print(count_states_action)
             reward = 1
             walking = False
             
@@ -331,25 +283,48 @@ def SARSAmax(policy,count_states_action,count_good ,count_bad):
         state_ind = states[state["y"],state["x"]]
         action = new_action
         
-    return policy,count_good,count_bad,count_states_action
+    return policy,count_states_action
 
-policy = np.ones((4,25))
-debug = False
-count_good = 0
-count_bad = 0
-count_states_action = np.zeros((4,25))
-for i in range(100000):
-    if debug:
-        print("")
-        print(i)
-        print(policy)
-    policy,count_good,count_bad,count_states_action = SARSAmax(policy,count_states_action,count_good, count_bad)
+if __name__ == "__main__":
+        
+    ######Types available: "MonteCarlo", "SARSA", "SARSAlambda", "SARSAmax"
+    type_eval = "SARSAmax"
+    
+    numIter = 100000
+    policy = np.ones((4,25))/4
+    
+    #Needs debuging
+    if type_eval == "MonteCarlo":
+        find_0_array_vec = np.vectorize(find_0_array)
+        for i in range(numIter):
+            policy = MC_walk(policy,find_0_array_vec)
+    
+        result_policy = nice_display(policy) 
 
-policy = normalize_policy(policy)
-
-
-
-
+    elif type_eval == "SARSA":
+        count_states_action = np.zeros((4,25))
+        for i in range(numIter):
+            policy = SARSA_eval(policy,count_states_action)
+            
+        result_policy = nice_display(policy) 
+    
+    elif type_eval == "SARSAlambda":
+        lambda_ = 0.9
+        count_states_action = np.zeros((4,25))
+        for i in range(numIter):
+            policy,count_states_action = SARSA_lambda_eval(policy,lambda_,count_states_action)
+        
+        result_policy = nice_display(policy) 
+        
+    elif type_eval == "SARSAmax":
+        count_states_action = np.zeros((4,25))
+        for i in range(numIter):
+            policy,count_states_action = SARSAmax(policy,count_states_action)
+        
+        result_policy = nice_display(policy) 
+   
+    else:
+        print("Error: Choose an available method")
 
 
 
